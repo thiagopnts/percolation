f192c3adc4e659a6b43e01c36a3feb587327c53e
thiagopnts <thiagopnts@gmail.com>
2014-09-23 00:51:33 -0300

    add percolation draft

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::vec::Vec;
+use self::percolation::Percolation;
 
 mod percolation;
 
 fn main() {
-    let p = percolation::Percolation::new(50);
+    let mut p = Percolation::new(7);
+    p.open(0, 3);
+    p.open(1, 3);
+    p.open(2, 3);
+    p.open(4, 3);
+    p.open(5, 3);
+    p.open(6, 3);
     p.print_grid();
+    println!("percolates? {}", p.percolates());
 }
--- src/percolation.rs
+++ src/percolation.rs
@@ -1,5 +1,10 @@
 use std::vec::Vec;
 use std::fmt::{Show, Formatter, Result};
+use self::dynamic_connectivity::{QuickFind, QuickUnion, WeightedQuickUnion};
+
+//how do I remove this?
+#[path = "dynamic_connectivity/mod.rs"]
+mod dynamic_connectivity;
 
 enum State {
     Open,
@@ -19,18 +24,55 @@ impl Show for State {
 
 pub struct Percolation {
     n: uint,
-    list: Vec<State>,
-    positions: Vec<uint>
+    states: Vec<State>,
+    pub positions: QuickFind
 }
 
 impl Percolation {
     pub fn new(n: uint) -> Percolation {
-        Percolation{n: n, list: Vec::from_fn(n * n, |_| Blocked), positions: Vec::from_fn(n * n, |i| i)}
+        Percolation{n: n, states: Vec::from_fn(n * n, |_| Blocked), positions: QuickFind::new(n * n)}
+    }
+
+    fn to_index(&self, i: uint, j: uint) -> uint {
+        i * self.n + j
+    }
+
+    pub fn open(&mut self, i: uint, j: uint) {
+        let index = self.to_index(i, j);
+        *self.states.get_mut(index) = Open;
+        self.positions.union(i, index);
+    }
+
+    pub fn is_open(&self, i: uint, j: uint) -> bool {
+        match self.states[self.to_index(i, j)] {
+            Open => true,
+            _    => false
+        }
+    }
+
+    pub fn is_full(&self, i: uint, j: uint) -> bool {
+        match self.states[self.to_index(i, j)] {
+            Full => true,
+            _    => false
+        }
+    }
+
+    pub fn percolates(&self) -> bool {
+        let lower_row_start = (self.n * self.n) - self.n;
+        let lower_row_end = self.n * self.n;
+        for i in range(0, self.n) {
+            for j in range(lower_row_start, lower_row_end) {
+                if self.positions.connected(i, j) {
+                    return true;
+                }
+            }
+        }
+        false
     }
 
     pub fn print_grid(&self) {
-        for i in range(1, self.list.len() + 1) {
-            print!("{}  ", self.list[i - 1]);
+        for i in range(1, self.states.len() + 1) {
+            print!("{}  ", self.states[i - 1]);
             if i % self.n == 0 { println!(""); }
         }
     }
