06b5d10e9f669f5464e70a0d16fec2bcd22a9b4d
thiagopnts <thiagopnts@gmail.com>
2014-09-23 00:50:58 -0300

    add dynamic connectivity algorithms as a module

---
+++ src/dynamic_connectivity/quick_find.rs
@@ -0,0 +1,26 @@
+
+use std::vec::Vec;
+
+pub struct QuickFind {
+    id: Vec<int>
+}
+
+impl QuickFind {
+    pub fn new(n: uint) -> QuickFind {
+        QuickFind{ id: Vec::from_fn(n, |i| i as int) }
+    }
+
+    pub fn connected(&self, p: uint, q: uint) -> bool {
+        self.id[p] == self.id[q]
+    }
+
+    pub fn union(&mut self, p: uint, q: uint) {
+        let pid = self.id[p];
+        let qid = self.id[q];
+        for i in range(0, self.id.len()) {
+            if self.id[i] == pid {
+                *self.id.get_mut(i) = qid;
+            }
+        }
+    }
+}
